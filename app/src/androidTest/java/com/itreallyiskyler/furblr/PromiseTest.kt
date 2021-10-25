a106a07459f146692c1f8044eccd9b5a0a4e2731
Kylaaa <kyler.mulherin@gmail.com>
2021-10-24 18:33:20 -0700

    Refactor to Promises

---
+++ app/src/androidTest/java/com/itreallyiskyler/furblr/PromiseTest.kt
@@ -0,0 +1,4 @@
+package com.itreallyiskyler.furblr
+
+class PromiseTest {
+}
---
+++ app/src/main/java/com/itreallyiskyler/furblr/util/Promise.kt
@@ -0,0 +1,119 @@
+package com.itreallyiskyler.furblr.util
+
+import kotlin.concurrent.thread
+
+enum class PromiseState {
+    Pending,
+    Started,
+    Resolved,
+    Rejected,
+}
+typealias GenericCallback = (Any)->Any?
+
+class Promise(action: (resolve: GenericCallback, reject: GenericCallback) -> Unit) {
+    private var _promiseState : PromiseState = PromiseState.Pending
+    private lateinit var _promiseValue : Any
+    private var _successObservers : MutableList<GenericCallback> = mutableListOf()
+    private var _failureObservers : MutableList<GenericCallback> = mutableListOf()
+
+    init {
+        thread(start=true) {
+            try {
+                _promiseState = PromiseState.Started
+                action(::_resolver, ::_rejector)
+            } catch (ex: Exception) {
+                reject(ex)
+            }
+        }
+    }
+    private fun _resolver(resolvedValue : Any){
+        if (_promiseState != PromiseState.Started) {
+            // cannot resolve multiple times
+            return
+        }
+
+        _promiseValue = resolvedValue
+        _promiseState = PromiseState.Resolved
+
+        for (callback in _successObservers) {
+            callback(_promiseValue)
+        }
+    }
+    private fun _rejector(rejectedValue : Any) {
+        if (_promiseState != PromiseState.Started) {
+            // cannot reject multiple times
+            return
+        }
+
+        _promiseValue = rejectedValue
+        _promiseState = PromiseState.Rejected
+    }
+    private fun _createAdvancer(action : GenericCallback,
+                                resolve : GenericCallback,
+                                reject : GenericCallback) : GenericCallback{
+        val advancer = fun(futurePromiseValue : Any) {
+            var result : Any?
+            try {
+                result = action(futurePromiseValue)
+            }
+            catch (ex : Exception) {
+                reject(ex)
+                return
+            }
+
+            resolve(result!!)
+        }
+        return advancer
+    }
+
+    fun then(successHandler : GenericCallback?, failureHandler : GenericCallback?) : Promise {
+        val action = fun(resolve : GenericCallback, reject : GenericCallback) {
+            var successCallback = resolve
+            if (successHandler != null) {
+                successCallback = _createAdvancer(successHandler, resolve, reject)
+            }
+
+            var failureCallback = reject
+            if (failureHandler != null) {
+                failureCallback = _createAdvancer(failureHandler, resolve, reject)
+            }
+
+            if (_promiseState == PromiseState.Resolved) {
+                successCallback(_promiseValue)
+            }
+            else if(_promiseState == PromiseState.Rejected) {
+                failureCallback(_promiseValue)
+            }
+            else {
+                _successObservers.add(successCallback)
+                _failureObservers.add(failureCallback)
+            }
+        }
+
+        return Promise(action)
+    }
+
+    fun catch(failureHandler : GenericCallback) : Promise {
+        return then(null, failureHandler)
+    }
+
+    companion object{
+        fun resolve(value : Any) : Promise {
+            val action = fun(resolve : GenericCallback, _ : GenericCallback) {
+                resolve(value)
+            }
+            return Promise(action)
+        }
+
+        fun reject(value : Any) : Promise {
+            val action = fun(_ : GenericCallback, reject : GenericCallback) {
+                reject(value)
+            }
+            return Promise(action)
+        }
+
+        fun all(promises : Array<Promise>){
+            throw NotImplementedError("This is not yet ready.")
+        }
+    }
+}
